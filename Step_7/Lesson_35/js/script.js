67068553186ca3c43988d1e24fbc641d7448bee7
Nikita <turrtopsis@gmail.com>
2021-05-15 10:16:36 +0300

    add Step 7

---
+++ Step_7/Lesson_35/js/script.js
@@ -0,0 +1,17 @@
+// Динамическое формирование скриптов
+// defer, async
+
+"use strict";
+
+const p = document.querySelectorAll('p');
+
+console.log(p);
+
+function loadScript (src){
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
+loadScript("js/test.js");
+loadScript("js/some.js");
