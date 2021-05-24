882599e0e50c9c21afc452abd5d56d0be66eac05
Nikita <turrtopsis@gmail.com>
2021-05-24 23:21:07 +0300

    Add moddal

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -93,6 +93,37 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setClock('.timer', deadLine);
 
+    //Modal
+  
+    const   modal = document.querySelector('.modal'),
+            btnModal = document.querySelectorAll('[data-modal]'),
+            btnModalClose = document.querySelector('[data-close]');
+
+
+    function closeModal () {
+        modal.classList.toggle('show');
+        document.body.style.overflow = "";
+    }        
+    btnModal.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.toggle('show');
+            document.body.style.overflow = "hidden";
+        });
+    }); 
+    btnModalClose.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e)=> {
+        if (e.target == modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code == "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
 });
 
 
