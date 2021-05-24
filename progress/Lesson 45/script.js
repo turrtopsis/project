40b47b4d5bda7ec7cd723ddbad32cfbe32506d09
Nikita <turrtopsis@gmail.com>
2021-05-25 00:05:23 +0300

    add lesson 45

---
+++ progress/Lesson 45/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//Функции конструкторы
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`hello ${this.name}`);
+    };
+}
+
+
+
+User.prototype.exit = function(name) {
+    console.log(`Пользователь ${this.name} ушел`);
+};
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
