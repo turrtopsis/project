f6352af414126b4bda0e464f962700262df1b7a3
Nikita <turrtopsis@gmail.com>
2021-05-25 11:59:27 +0300

    add Lesson 49

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -127,7 +127,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // const modalTimerId = setTimeout(openModal, 5000);
+    const modalTimerId = setTimeout(openModal, 5000);
 
     function showModalByScroll() {
         if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
