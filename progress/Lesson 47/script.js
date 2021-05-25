c4ab4b2fa26505abc7f14ebd00f4fe3ad2ef0b2a
Nikita <turrtopsis@gmail.com>
2021-05-25 10:31:18 +0300

    add Lesson 47

---
+++ progress/Lesson 47/script.js
@@ -0,0 +1,37 @@
+//Классы. синтаксический сахар
+
+'use strict';
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+class ColorRedRectangleWithText extends Rectangle {
+    constructor (height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+    }
+}
+
+const div = new ColorRedRectangleWithText(25, 10, 'Hello World', 'red');
+
+div.showMyProps();
+console.log(div.calcArea());
+
+// const square = new Rectangle (10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(long.calcArea());
+// console.log(square.calcArea());
